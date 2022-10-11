38c90908ded3c1a3bb4e8189994df9cafb00bfeb
Mark Lagana <info@technisculpt.com>
2022-10-11 17:27:28 +1100

    add player collisions

---
+++ public/scripts/common.js
@@ -0,0 +1,22 @@
+export function check_vacancy(bounds, grid_size)
+{
+    let range_x = (bounds[1] - bounds[0])/grid_size;
+    let range_y = (bounds[3] - bounds[2])/grid_size;
+    
+    while(1)
+    {
+        let rowX = Math.floor(Math.random() * range_x) + Math.floor(bounds[0]/grid_size);
+        let rowY = Math.floor(Math.random() * range_y) + Math.floor(bounds[2]/grid_size);
+
+        if (document.grid.grid[rowX][rowY])
+        {
+            
+            continue;
+        }
+        else
+        {
+            return [rowX, rowY];
+        } 
+    }
+
+}
--- public/scripts/player.js
+++ public/scripts/player.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { TWEEN } from '../jsm/libs/tween.module.min.js'
+import {check_vacancy} from './common.js'
 
 export default class Player
 {
@@ -18,11 +19,15 @@ export default class Player
         this.grid_spaces = Math.floor(width/grid_size);
         const geometry = new THREE.CylinderGeometry(this.grid_size/2, this.grid_size/2, this.grid_size, 20);
         const material = new THREE.MeshBasicMaterial({color: colour})
+        let new_pos = null;
+        if (player_no == 1) {new_pos = check_vacancy([0, width/2, 0, height/2], grid_size);}
+        if (player_no == 2) {new_pos = check_vacancy([width/2, width, height/2, height], grid_size);}
         this.player_mesh = new THREE.Mesh(geometry, material)
-        this.player_mesh.position.x = grid_size - width/2
-        this.player_mesh.position.y = grid_size - height/2
+        this.player_mesh.position.x = (new_pos[0]  * grid_size) - width/2;
+        this.player_mesh.position.y = (new_pos[1] * grid_size)  - height/2;
         this.player_mesh.position.z += grid_size
         this.player_mesh.rotation.x += Math.PI/2
+        document.grid.grid[new_pos[0]][new_pos[1]] = player_no + 1;
         this.scene = scene
         this.scene.add(this.player_mesh)
         this.keys = keys;
@@ -41,38 +46,44 @@ export default class Player
 
     logKey = (e) =>
     {
+
+        if (!this.keys.includes(e.code)) return;
+
         if(!this.debounce)
         {
             let x_pos = Math.floor((this.player_mesh.position.x + this.width/2)/this.grid_size);
             let y_pos = Math.floor((this.player_mesh.position.y + this.height/2)/this.grid_size);
-
-            if (!this.keys.includes(e.code)) return;
 
             let delta_x = this.key_lookup[e.code][0];
             let delta_y = this.key_lookup[e.code][1];
             
             if ((x_pos + delta_x >= this.grid_spaces) || (x_pos + delta_x < 0) || // player bounds check
             (y_pos + delta_y >= this.grid_spaces) || (y_pos + delta_y < 0)) return;
-            
 
-            if (!document.grid.grid[x_pos + delta_x][y_pos + delta_y]) // no block in the way of player
+            if (document.grid.grid[x_pos + delta_x][y_pos + delta_y] > 1) return; // player infront
+
+            if (document.grid.grid[x_pos + delta_x][y_pos + delta_y] == 0) // no block in the way of player
             {
                 this.debounce = true;
                 setTimeout(this.set_debounce, this.tween_duration1 + this.debounce_ms);
+                document.grid.grid[x_pos][y_pos] = 0;
+                document.grid.grid[x_pos + delta_x][y_pos + delta_y] = this.player_no + 1;
                 new TWEEN.Tween(this.player_mesh.position).to({
                     x: this.player_mesh.position.x + delta_x * this.grid_size,
                     y: this.player_mesh.position.y + delta_y * this.grid_size},this.tween_duration1).start();
             }
-            
+
             else if ((x_pos + 2 * delta_x >= this.grid_spaces) || (x_pos + 2 * delta_x < 0) || // box bounds check
             (y_pos + 2 * delta_y >= this.grid_spaces) || (y_pos + 2 * delta_y < 0)) return;
             
-            else if(!document.grid.grid[x_pos + 2 * delta_x][y_pos + 2 * delta_y]) // block in the way of player but no block behind it
+            else if(document.grid.grid[x_pos + 2 * delta_x][y_pos + 2 * delta_y] == 0) // block in the way of player but no block behind it
             {
                 this.debounce = true;
                 setTimeout(this.set_debounce, this.tween_duration2 + this.debounce_ms);
                 document.grid.grid[x_pos + delta_x][y_pos + delta_y] = 0;
                 document.grid.grid[x_pos + 2 * delta_x][y_pos + 2 * delta_y] = 1;
+                document.grid.grid[x_pos][y_pos] = 0;
+                document.grid.grid[x_pos + delta_x][y_pos + delta_y] = this.player_no + 1;
                 let selected_box = this.scene.getObjectByName(String(x_pos + delta_x) + ',' + String(y_pos + delta_y));
                 selected_box.name = String(x_pos + 2 * delta_x) + ',' + String(y_pos + 2 * delta_y);
                 new TWEEN.Tween(selected_box.position).to({
@@ -86,12 +97,15 @@ export default class Player
             else if ((x_pos + 3 * delta_x >= this.grid_spaces) || (x_pos + 3 * delta_x < 0) || // box2 bounds check
             (y_pos + 3 * delta_y >= this.grid_spaces) || (y_pos + 3 * delta_y < 0)) return;
 
-            else if (!document.grid.grid[x_pos + 3 * delta_x][y_pos + 3 * delta_y]) // 2 blocks in the way but no block behind it 
+            else if (document.grid.grid[x_pos + 3 * delta_x][y_pos + 3 * delta_y] == 0) // 2 blocks in the way but no block behind it 
             {
+                if(document.grid.grid[x_pos + 2 * delta_x][y_pos + 2 * delta_y] > 1) return; // other player 2 blocks behind
                 this.debounce = true;
                 setTimeout(this.set_debounce, this.tween_duration3 + this.debounce_ms);
                 document.grid.grid[x_pos + delta_x][y_pos + delta_y] = 0;
                 document.grid.grid[x_pos + 3 * delta_x][y_pos + 3 * delta_y] = 1;
+                document.grid.grid[x_pos][y_pos] = 0;
+                document.grid.grid[x_pos + delta_x][y_pos + delta_y] = this.player_no + 1;
                 let selected_box1 = this.scene.getObjectByName(String(x_pos + delta_x) + ',' + String(y_pos + delta_y));
                 let selected_box2 = this.scene.getObjectByName(String(x_pos + 2 * delta_x) + ',' + String(y_pos + 2 * delta_y));
                 selected_box1.name = String(x_pos + 2 * delta_x) + ',' + String(y_pos + 2 * delta_y);

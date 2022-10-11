b764cba1ebb5d2dccf290aa7d05f7cbf0d8e6ff2
Mark Lagana <info@technisculpt.com>
2022-10-11 17:34:04 +1100

    fix bounds bug

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -1,16 +1,16 @@
 export function check_vacancy(bounds, grid_size)
 {
-    let range_x = (bounds[1] - bounds[0])/grid_size;
-    let range_y = (bounds[3] - bounds[2])/grid_size;
+    let range_x = (bounds[1] - bounds[0])/grid_size - grid_size;
+    let range_y = (bounds[3] - bounds[2])/grid_size - grid_size;
     
     while(1)
     {
         let rowX = Math.floor(Math.random() * range_x) + Math.floor(bounds[0]/grid_size);
         let rowY = Math.floor(Math.random() * range_y) + Math.floor(bounds[2]/grid_size);
-
+        console.log(rowX, rowY);
+        console.log(document.grid.grid[rowX][rowY])
         if (document.grid.grid[rowX][rowY])
         {
-            
             continue;
         }
         else
